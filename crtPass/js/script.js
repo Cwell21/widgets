93ac297102b58c21c5ea721d263825d0095e5989
Cwell21 <cwe11@yahoo.com>
2019-05-04 06:38:49 -0600

    Pass certification widget working.  TODO: add more functionality & responsive design

--- crtPass/js/script.js
+++ crtPass/js/script.js
@@ -7,6 +7,7 @@ const elClear = document.getElementById("btnClr")
 const elLower = document.getElementById("lowerCase");
 const elUpper = document.getElementById("upperCase");
 const elNumber = document.getElementById("number");
+const elLength = document.getElementById("length");
 const elMessage = document.getElementById("message");
 const isGood = document.getElementsByClassName("good");
 const isBad = document.getElementsByClassName("bad");
@@ -17,31 +18,35 @@ const lower = /[a-z]/;
 const upper = /[A-Z]/;
 const num = /[0-9]/;
 
-
+//WHEN USER CLICKS IN PASSWORD INPUT, DISPLAY REQUIREMENTS
 elPassword.onfocus = () => {
   elMessage.style.display = "block";
 }
 
+//IF USER LEAVES PASSWORD INPUT WITH OUT INPUT, HID REQUIREMENTS
 elPassword.onblur = () => {
   if(elPassword.value.length < 1){
   elMessage.style.display = "none";
   }
 }
 
+//CHECK INPUT VS REQUIRED REG EXP TO VALIDATE INPUT
 elPassword.addEventListener("keyup", event => {
+  
+  //CHECK FOR LOWER CASE VALUE
   if (elPassword.value.match(lower)) {
     elLower.removeAttribute("class");
     elLower.setAttribute("class", "valid");
     isBad[0].style.display = "none";
     isGood[0].style.display = "inline";
 
-  }else {
+  }else { 
     elLower.removeAttribute("class");
     elLower.setAttribute("class", "invalid");
     isGood[0].style.display = "none";
-    isBad[0].style.display = "inline"
+    isBad[0].style.display = "inline";
   }
-
+  //CHECK FOR UPPER CASE VALUE
   if (elPassword.value.match(upper)) {
     elUpper.removeAttribute("class");
     elUpper.setAttribute("class", "valid");
@@ -51,8 +56,9 @@ elPassword.addEventListener("keyup", event => {
     elUpper.removeAttribute("class");
     elUpper.setAttribute("class", "invalid");
     isGood[1].style.display = "none";
-    isBad[1].style.display = "inline"
+    isBad[1].style.display = "inline";
   }
+  //CHECK FOR NUMBER VALUE
   if (elPassword.value.match(num)) {
     elNumber.removeAttribute("class");
     elNumber.setAttribute("class", "valid");
@@ -62,7 +68,19 @@ elPassword.addEventListener("keyup", event => {
     elNumber.removeAttribute("class");
     elNumber.setAttribute("class", "invalid");
     isGood[2].style.display = "none";
-    isBad[2].style.display = "inline"
+    isBad[2].style.display = "inline";
+  }
+  //CHECK FOR VALID LENGTH
+  if (elPassword.value.length >= 8) {
+    elLength.removeAttribute("class");
+    elLength.setAttribute("class", "valid");
+    isBad[3].style.display = "none";
+    isGood[3].style.display = "inline";
+  }else {
+    elLength.removeAttribute("class");
+    elLength.setAttribute("class", "invalid");
+    isGood[3].style.display = "none";
+    isBad[3].style.display = "inline";
   }
   
 })
@@ -87,6 +105,7 @@ elClear.addEventListener("click", event => {
   elUpper.setAttribute("class", "invalid");
   elNumber.removeAttribute("class");
   elNumber.setAttribute("class", "invalid");
+  elLength.setAttribute("class", "invalid");
   for (let i=0; i < isBad.length; i++) {
     isBad[i].style.display = "inline";
     isGood[i].style.display = "none";
