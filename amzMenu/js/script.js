1c977b20069584ebbdfd92e00c571700dfb5e449
Cwell21 <cwe11@yahoo.com>
2019-05-12 06:57:29 -0600

    Progress - working on sub menu.  HTML created, need to add css and javascript

--- amzMenu/js/script.js
+++ amzMenu/js/script.js
@@ -1,7 +1,15 @@
+
 (function() {
 
+  //CONSTANTS TO HOLD DOM ELEMENTS FOR MENU
   const btnMenu = document.getElementById("btnMenu");
   const mnuAside = document.getElementById("modalMenu");
+  const mnuSub = document.getElementById("subMenu");
+
+  function creteSubMenu(mnu) {
+    
+  }
+
 
   btnMenu.addEventListener("click", () => {
     mnuAside.style.left = 0;
